9162a2748f5b6080c16a99b36c1a0f0c42d8d7da
Dipanshu <194807518+DipanshukrGit@users.noreply.github.com>
2026-06-17 16:10:42 +0530

    Remove unnecessary empty line in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import { ArrowRight, Check, Stethoscope } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-
+//
 export default function Home() {
   return (
    <div className="bg-background">
